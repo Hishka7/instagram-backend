d9f1cf6057d31f0baffc58cfed9b3f1c1dfce99b
24HP3949 24HP3949 <24HP3949@admins-iMac.local>
2024-12-28 13:00:45 +0800

    comments

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -2,6 +2,7 @@ const Route = require("express");
 const commentRoute = Route();
 const commentModel = require("../models/commentSchema");
 const postModel = require("../models/postSchema");
+const { populate } = require("dotenv");
 
 commentRoute.post("/comments", async (req, res) => {
   const body = req.body;
@@ -15,14 +16,19 @@ commentRoute.post("/comments", async (req, res) => {
   res.send(createdComment);
 });
 
-commentRoute.get("/comments", async (req, res) => {
-  try {
-    const comments = await postModel.find().populate("comments");
-    res.send(comments);
-  } catch (error) {
-    console.log(error);
-    throw new Error("Failed to get posts");
-  }
+// commentRoute.get("/comments", async (req, res) => {
+//   try {
+//     const comments = await postModel.find(postId).populate("comments");
+
+//     res.send(comments);
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error("Failed to get posts");
+//   }
+// });
+commentRoute.get("/post/comments", async (req, res) => {
+  const postComments = await postModel.find(postId).populate("comments");
+  res.send(postComments);
 });
 
 module.exports = commentRoute;
